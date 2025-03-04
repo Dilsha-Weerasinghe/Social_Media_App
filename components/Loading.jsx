5fffbd62057a812c3c7ba981f8d2a422c263fff9
Dilsha Weerasinghe <dilshasachith1234@gmail.com>
2025-03-04 08:38:08 +0530

    Create Loading.jsx

---
+++ components/Loading.jsx
@@ -0,0 +1,21 @@
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { theme } from '../constants/theme';
+
+const Loading = ({ size = 'large', color = theme.colors.primary }) => {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size={size} color={color} />
+    </View>
+  );
+};
+
+export default Loading;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
