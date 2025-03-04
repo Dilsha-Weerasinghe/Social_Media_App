1fbb8bbdd3c6696d8850854fdc7e4f7960d523c9
Dilsha Weerasinghe <dilshasachith1234@gmail.com>
2025-03-04 08:40:26 +0530

    Create userService.js

---
+++ Services/userService.js
@@ -0,0 +1,37 @@
+import { supabase } from "../lib/supabase";
+
+export const getUserData= async(userId)=>{
+    try{
+        const{data,error}= await supabase
+        .from('users')
+        .select()
+        .eq('id',userId)
+        .single();
+        if(error){
+            return{succes:false,msg:error?.message}
+
+        }
+        return{succes:true,data};
+
+    }catch(error){
+        console.log('got error',error);
+        return{succes:false,msg:error.message}
+    }
+}
+export const updateUser= async(userId,data)=>{
+    try{
+        const{error}= await supabase
+        .from('users')
+        .update(data)
+        .eq('id',userId);
+        if(error){
+            return{succes:false,msg:error?.message}
+
+        }
+        return{succes:true,data};
+
+    }catch(error){
+        console.log('got error',error);
+        return{succes:false,msg:error.message}
+    }
+}
